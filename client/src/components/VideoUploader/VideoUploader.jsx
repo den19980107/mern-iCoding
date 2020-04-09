6b6020868a897e5df03763b1023d3cc626150d90
Paul <den19980107@gmail.com>
2020-04-09 09:41:43 +0800

    try fix 課程封面影片上傳失敗問題

--- client/src/components/VideoUploader/VideoUploader.jsx
+++ client/src/components/VideoUploader/VideoUploader.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
-import { Upload, Button, Icon } from 'antd';
+import { Upload, Button, Icon, message } from 'antd';
 const VideoUploader = ({ onChange, belongUnit, displayName, style }) => {
-   const props = {
-      action: '/api/video/upload',
-      onChange({ file, fileList }) {
-         if (file.status !== 'uploading') {
-            if (file.xhr.status == 200) {
-               onChange(file.response.videoUrl)
+    const props = {
+        action: '/api/video/upload',
+        onChange({ file, fileList }) {
+            if (file.status !== 'uploading') {
+                if (file.xhr.status == 200) {
+                    message.success(file.response.message)
+                    onChange(file.response.videoUrl)
+                }
             }
-         }
-      },
-      accept: 'video/*',
-      defaultFileList: [],
-      data: {}
-   };
-   if (belongUnit) {
-      props.data.belongUnitId = belongUnit
-   }
-   if (displayName) {
-      props.data.displayName = displayName
-   }
-   return (
-      <Upload {...props} >
-         <button style={{ fontSize: "20px", padding: "0.5rem", backgroundColor: "white", border: "0.5px solid #ccc", ...style }}>
-            <Icon type="upload" /> 上傳影片
+        },
+        accept: 'video/*',
+        defaultFileList: [],
+        data: {}
+    };
+    if (belongUnit) {
+        props.data.belongUnitId = belongUnit
+    }
+    if (displayName) {
+        props.data.displayName = displayName
+    }
+    return (
+        <Upload {...props} >
+            <button style={{ fontSize: "20px", padding: "0.5rem", backgroundColor: "white", border: "0.5px solid #ccc", ...style }}>
+                <Icon type="upload" /> 上傳影片
          </button>
-      </Upload>
-   );
+        </Upload>
+    );
 };
 
 export default VideoUploader;
