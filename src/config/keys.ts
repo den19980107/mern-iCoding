a98acc96ec1735381dfbaa62417159ed227ea3e5
Paul <den19980107@gmail.com>
2020-04-10 08:42:50 +0800

    修改預設部署資料庫

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,28 +1,28 @@
 import config from '../config/default'
 const FACEBOOK = {
-  clientID: "2680354318696556",
-  clientSecret: "990e5a01902ce47d0a3dbc3ecb6704e3"
+    clientID: "2680354318696556",
+    clientSecret: "990e5a01902ce47d0a3dbc3ecb6704e3"
 }
 
 const GOOGLE = {
-  clientID: "816197021536-f3knspgc3nvugb5soejmkir7amgknqki.apps.googleusercontent.com",
-  clientSecret: "6j4siYsuV8tozj_geVLlycLG"
+    clientID: "816197021536-f3knspgc3nvugb5soejmkir7amgknqki.apps.googleusercontent.com",
+    clientSecret: "6j4siYsuV8tozj_geVLlycLG"
 }
 
 const SESSION = {
-  COOKIE_KEY: "user"
+    COOKIE_KEY: "user"
 };
 
 const localDB = 'mongodb://localhost:27017/mern-iCoding';
-const productionDB = 'mongodb://icoding:icoding19980107@ds363038.mlab.com:63038/icoding'
+const productionDB = 'mongodb://localhost:27017/mern-iCoding'
 const DBUrl = config.mode == "production" ? productionDB : localDB
 const MONGODB = {
-  MONGODB_URI: DBUrl
+    MONGODB_URI: DBUrl
 };
 
 export = {
-  FACEBOOK,
-  GOOGLE,
-  SESSION,
-  MONGODB
+    FACEBOOK,
+    GOOGLE,
+    SESSION,
+    MONGODB
 }
